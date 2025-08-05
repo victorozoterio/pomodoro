dd7a151f977c43fdb7ffd81942b227903fe034c9
Victor Ozoterio <victorozoterio@gmail.com>
2025-08-05 01:01:05 -0300

    style: biome.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App.tsx';
 
+// biome-ignore lint/style/noNonNullAssertion: Ignoring because 'root' element is always present in the DOM.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
