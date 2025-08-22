0f45dd6fec2591be08f4761388ae42e95dd31914
Victor Ozoterio <victorozoterio@gmail.com>
2025-08-22 16:25:50 -0300

    feat: adiciona funcionalidade ao formulário sem usar biblioteca

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,14 +8,23 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles';
+import { useState } from 'react';
 
 export function Home() {
+  const [task, setTask] = useState('');
+
   return (
     <HomeContainer>
       <form action=''>
         <FormContainer>
           <label htmlFor='task'>Vou trabalhar em</label>
-          <TaskInput id='task' list='task-suggestions' placeholder='Dê um nome para o seu projeto' />
+          <TaskInput
+            id='task'
+            list='task-suggestions'
+            placeholder='Dê um nome para o seu projeto'
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
 
           <datalist id='task-suggestions'>
             <option value='Projeto 1' />
@@ -39,7 +48,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type='submit'>
+        <StartCountdownButton disabled={!task} type='submit'>
           <Play size={24} />
           Começar
         </StartCountdownButton>
