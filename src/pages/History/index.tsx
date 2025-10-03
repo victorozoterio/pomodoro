ae0afc13297a8e0d5f35a4642f1532a6438abf3e
Victor Ozoterio <victorozoterio@gmail.com>
2025-10-02 23:35:19 -0300

    feat: formata data de criação

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { CyclesContext } from '../../contexts/CyclesContext';
 import { HistoryContainer, HistoryList, Status } from './styles';
 
@@ -25,7 +27,7 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>{formatDistanceToNow(cycle.startDate, { addSuffix: true, locale: ptBR })}</td>
                   <td>
                     {cycle.finishedDate && <Status statusColor='green'>Concluído</Status>}
                     {cycle.interruptedDate && <Status statusColor='red'>Interrompido</Status>}
